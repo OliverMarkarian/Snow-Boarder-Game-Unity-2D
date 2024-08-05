c4be35a176c7ce447c90709b70e74bacc744e0b6
Oliver <oliver.markarian@gmail.com>
2024-08-05 18:29:55 +0200

    Update CrashDetector.cs

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other) 
-    {
+    private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Ground")
         {
             Debug.Log("Ouch, hit my head");
-        }    
+        }
     }
 }
