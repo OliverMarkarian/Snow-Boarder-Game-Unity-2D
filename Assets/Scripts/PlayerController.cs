72428b31cf321d18e921150418a706237728ddaf
Oliver <oliver.markarian@gmail.com>
2024-08-05 17:56:55 +0200

    Add Rotation functionality for snowboarder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb2d;
+    [SerializeField] float torqueSpeed = 40f;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(torqueSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddTorque(-torqueSpeed);
+        }
     }
 }
