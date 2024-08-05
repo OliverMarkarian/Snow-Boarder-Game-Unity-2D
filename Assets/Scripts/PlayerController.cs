e97f9d68ddfbeedeb9d5c6e3afc454bb5f935f24
Oliver <oliver.markarian@gmail.com>
2024-08-05 23:52:00 +0200

    Boost speed implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,12 +8,35 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float torqueSpeed = 40f;
+    [SerializeField] float boostSpeed = 60f;
+    [SerializeField] float baseSpeed = 40f;
+    SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
     void Update()
+    {
+        RotatePlayer();
+        RespondToboost();
+    }
+
+    void RespondToboost()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+            
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
