25cb73887b9a17264262585ba91f8a3aaa7cb22c
Oliver <oliver.markarian@gmail.com>
2024-08-05 22:10:58 +0200

    Update CrashDetector.cs

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 0.2f;
         void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Ground")
         {
             Debug.Log("Ouch, hit my head");
-            Invoke("reloadScene",0.2f);
+            Invoke("reloadScene",loadDelay);
             
         }   
     }
